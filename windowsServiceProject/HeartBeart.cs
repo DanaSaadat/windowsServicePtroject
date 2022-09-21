1606f0c7d735d2c9960cfbc4ce9707f54a2f2aa8
DanaSaadat <danasaadat2022@gmail.com>
2022-09-21 16:19:31 +0300

    First Commit

---
+++ windowsServiceProject/HeartBeart.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace windowsServiceProject
+{
+    public class HeartBeart
+    {
+        private readonly Timer _timer;
+        public HeartBeart()
+        {
+            _timer = new System.Timers.Timer(2000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] LINES = new string[] { DateTime.Now.ToString() };
+            //string[] LINES = new string[] { "dana","saadat" };
+            //File.AppendAllLines(@"C:\Temp\demo\HeartBeart.txt", LINES);
+            File.AppendAllLines(@"C:\Temp\Demo\danaNote.txt", LINES);
+        }
+       
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+    }
+}
---
+++ windowsServiceProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace windowsServiceProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<HeartBeart>(s =>
+                {
+                    s.ConstructUsing(service => new HeartBeart());
+
+                    s.WhenStarted(service => service.Start());
+                    s.WhenStopped(service => service.Stop());
+
+                });
+                x.RunAsLocalSystem();
+                x.SetServiceName("HeartBeartService");
+                x.SetDisplayName("HeartBeart Service");
+                x.SetDescription("This is sample HeartBeart Service used in a youtube demo");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+
+
+        }
+    }
+}
